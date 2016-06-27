14001c9e462a4d3e47cd508ed5d1b60a8d2e2a5a
Nick Maskell <nrmaskell@gmail.com>
2016-06-27 20:43:14 +1200

    Working

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ let paddleX = (canvas.width - paddleWidth) / 2
 let rightPressed = false
 let leftPressed = false
 
+const brickRowCount = 3
+const brickColumnCount = 5
+const brickWidth = 75
+const brickHeight = 20
+const brickPadding = 10
+const brickOffsetTop = 30
+const brickOffsetLeft = 30
+
+const bricks = []
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = []
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 }
+  }
+}
+
 function drawBall () {
   ctx.beginPath()
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
@@ -29,23 +45,62 @@ function drawPaddle () {
   ctx.closePath()
 }
 
+function drawBricks () {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for(let r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status === 1) {
+        const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft
+        const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop
+        bricks[c][r].x = brickX
+        bricks[c][r].y = brickY
+        ctx.beginPath()
+        ctx.rect(brickX, brickY, brickWidth, brickHeight)
+        ctx.fillStyle = '#0095DD'
+        ctx.fill()
+        ctx.closePath()
+      }
+    }
+  }
+}
+
 function draw () {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall()
   drawPaddle()
+  drawBricks()
+  collisionDetection()
   x += dx
   y += dy
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx
-  }
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+  } if (y + dy < ballRadius) {
     dy = -dy
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy
+    } else {
+      alert('GAME OVER')
+      document.location.reload()
+    }
   }
   if (rightPressed && paddleX < canvas.width - paddleWidth) {
     paddleX += 7
-  }
-  if (leftPressed && paddleX > 0) {
+  } if (leftPressed && paddleX > 0) {
     paddleX -= 7
+  }
+}
+
+function collisionDetection () {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      let b = bricks[c][r]
+      if (b.status === 1) {
+        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy
+          b.status = 0
+        }
+      }
+    }
   }
 }
 
