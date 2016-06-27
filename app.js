ffe1b5d1eb202c76a63abfacd610ea28c62aa807
Nick Maskell <nrmaskell@gmail.com>
2016-06-27 17:11:04 +1200

    add listeners

--- app.js
+++ app.js
@@ -3,18 +3,62 @@ const ctx = canvas.getContext('2d')
 
 let x = canvas.width / 2
 let y = canvas.height - 30
-const dx = 2
-const dy = -2
+let dx = 2
+let dy = -2
+const ballRadius = 10
 
-function draw () {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+const paddleHeight = 10
+const paddleWidth = 75
+let paddleX = (canvas.width - paddleWidth) / 2
+let rightPressed = false
+let leftPressed = false
+
+function drawBall () {
   ctx.beginPath()
-  ctx.arc(x, y, 10, 0, Math.PI * 2)
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
   ctx.fillStyle = '#0095DD'
   ctx.fill()
   ctx.closePath()
+}
+
+function drawPaddle () {
+  ctx.beginPath()
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
+  ctx.fillStyle = 'x0095DD'
+  ctx.fill()
+  ctx.closePath()
+}
+
+function draw () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBall()
   x += dx
   y += dy
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy
+  }
+}
+
+document.addEventListener('keydown', keyDownHandler, false)
+document.addEventListener('keyup', keyUpHandler, false)
+
+function keyDownHandler (e) {
+  if (e.keyCode === 39) {
+    rightPressed = true
+  } else if (e.keyCode === 37) {
+    leftPressed = true
+  }
+}
+
+function keyUpHandler (e) {
+  if (e.keyCode === 39) {
+    rightPressed = false
+  } else if (e.keyCode === 37) {
+    leftPressed = false
+  }
 }
 
 setInterval(draw, 10)
